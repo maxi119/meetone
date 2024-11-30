649d3b240765fa93f5db5b9a82ac567880ba38d0
maxi119 <joelin@mail2000.com.tw>
2024-11-30 23:02:14 +0800

    Update about.js

--- event/popup/about.js
+++ event/popup/about.js
@@ -32,12 +32,12 @@ function AboutPage() {
                 <h2>關於網頁</h2>
 
                 <p className="txt">這是一個使用 React 和 Bootstrap 建立的網頁。</p>
-                <p className="txt">使用 react 程 bootstrap 但不經過 babel 編譯，直接使用 import 引入。</p>
-                <p className="txt">使用上有一些限制，例如無法使用 JSX 語法，但可以使用 React 的函式式組件。</p>
-                <p className="txt">這樣可以在不 clone 下直接線上編輯 </p>
+                <p className="txt">使用 react 和 bootstrap 但不經過 babel 編譯，直接使用 import 引入。</p>
+                <p className="txt">使用上有一些限制，例如無法使用 JSX 語法，但可以使用 React 的函式組件。</p>
+                <p className="txt">這樣可以在直接線上編輯 </p>
                 <p className="txt">所有資訊皆無遠端存放，這是一個可以離線使用的網頁。</p>
                 <p className="txt">版權為本人所有，請勿商業使用。</p>
-                <p className="txt">如有問題，請聯絡：maxi.chang@gmail.com</p>
+                <p className="txt">如有問題，請聯絡：joelin@mail2000.com.tw</p>
             </div>
         </div>
     );
